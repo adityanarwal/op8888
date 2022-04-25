dd04256221932c1dc66e08cb749336965dc3a033
adityanarwal <93639647+adityanarwal@users.noreply.github.com>
2022-04-25 23:44:29 +0530

    Update var.py

--- var.py
+++ var.py
@@ -5,5 +5,5 @@ from os import getenv
 class Var:
     API_ID = config("API_ID", default=6, cast=int)
     API_HASH = config("API_HASH", None)
-    TELETHON_ID = config("TELETHON_ID", None)
+    TELETHON_ID = config("TELETHON_ID", "1AZWarzwBuxuU-AWQctktFInqpum4JIzVJaYSyKEVoxSZZSpXLTA-0LRbktoHCXz5mOQrEOLZanwdi4OBJgwKy-XmHyhQPqU1VJzxg2Co58N0MP-Ignrc7XJunTXPGCO2Zd7Zh0UchllrAP8fz1gja_5MxeARMMlEIWkS_hnVXyfbRBMGHVbPl3yADDq-ztoVbACFp9d-DeUhTFNx6I2MKBLDPW8ZyidH972lUiWZLynJmFs7kJwdy5jWGBeNRpX5LCOU2vgnA6TPMV5qHd6hX47DMBT5isUXyHYtiujNbMs1aS67Cg9UpI0avZgLPAIAYMvdUirnTo7pIiGLeaVvTZIOA4dnjcs=")
     SUDO = list(map(int, getenv("SUDO").split()))
